8b06dae7f9037c0ab8934f50121aee147783dd6c
daniel.faderski <daniel.faderski@jmr.pl>
2016-06-04 21:34:32 +0200

    fix

--- dilation_recontruction.m
+++ dilation_recontruction.m
@@ -3,8 +3,7 @@ function [ rec_image ] = dilation_recontruction( image, mask, se, loops )
 rec_image = image;
 
 for i=1: loops
-    rec_image = geo_dilation(mask, rec_image, mask, se); 
+    rec_image = geo_dilation(rec_image, mask, se); 
 end
 
 end
-
